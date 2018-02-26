7d6e33869148aa66dac3e40d05115f189333ada9
bitsngeeks <bits.n.geeks@gmail.com>
2018-02-26 09:14:31 -0500

    routing usersview

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -39,6 +39,10 @@ export const routes: Routes = [
       {
         path: 'charts',
         loadChildren: './chartjs/chartjs.module#ChartJSModule'
+      },
+      {
+        path: 'usersview',
+        loadChildren: './usersview/usersview.module#UsersviewModule'
       }
     ]
   },
@@ -50,33 +54,8 @@ export const routes: Routes = [
     },
     children: [
       {
-<<<<<<< HEAD
-        path: 'dashboard',
-        loadChildren: './dashboard/dashboard.module#DashboardModule'
-      },
-      {
-        path: 'usersview',
-        loadChildren: './usersview/usersview.module#UsersviewModule'
-      },
-      {
-        path: 'components',
-        loadChildren: './components/components.module#ComponentsModule'
-      },
-      {
-        path: 'icons',
-        loadChildren: './icons/icons.module#IconsModule'
-      },
-      {
-        path: 'widgets',
-        loadChildren: './widgets/widgets.module#WidgetsModule'
-      },
-      {
-        path: 'charts',
-        loadChildren: './chartjs/chartjs.module#ChartJSModule'
-=======
         path: 'development',
         loadChildren: './devtime/devtime.module#DevtimeModule'
->>>>>>> a6366ee27b872dbdab08b3b9cdf9870d006d4736
       }
     ]
   },
