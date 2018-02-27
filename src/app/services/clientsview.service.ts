b6768aba0f3cb2030f4246276c960b4e5f873a51
bitsngeeks <bits.n.geeks@gmail.com>
2018-02-27 11:42:51 -0500

    routes

--- src/app/services/clientsview.service.ts
+++ src/app/services/clientsview.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import { CommonModule } from "@angular/common";
 
 @Injectable()
 export class ClientsviewService {
 
-  constructor() { }
+  token="";
+  headers:Headers;
+  resultClients:Object[];
+  url='http://192.168.0.20:3000/api/clients';
+
+  constructor(private http:Http) { 
+    this.resultClients=[];
+  }
 
 }
